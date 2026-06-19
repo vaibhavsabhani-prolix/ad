cd74b543558fe4b98989246d8137682dd86e700b
vaibhav sabhani <vaibhavsabhani.prolix@gmail.com>
2026-06-19 10:19:42 +0530

    add the schema file in the location page

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -151,6 +151,29 @@ export default function GBPPage() {
             Contact Sales →
           </Link>
         </div>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  name: "Home",
+                  item: SITE_URL,
+                },
+                {
+                  "@type": "ListItem",
+                  position: 2,
+                  name: "Locations",
+                  item: `${SITE_URL}/locations`,
+                },
+              ],
+            }),
+          }}
+        />
 
         <script
           type="application/ld+json"
